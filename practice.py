bc11df23f4217a0406ac99faee88c40e0edd0749
Dean <o.michaeldean@gmail.com>
2022-09-30 17:34:34 +0100

    version

---
+++ practice.py
@@ -0,0 +1,9 @@
+x, y, z = 5, 6, "king"
+A, B, C = 90, 87, "COME"
+
+
+f = g = t = "king"
+print(t+"dom "+C)
+
+
+fruit = [5,6,5,3,5,3]
--- radius.py
+++ radius.py
@@ -1,11 +1,9 @@
 import math
-
-if __name__ == '__main__':
-    radius_str = input("Enter the radius of your circle: ")
+if __name__ == "__main__":
+    radius_str = input("Enter the radius of your circle: \n")
     radius_int = int(radius_str)
 
     circumference = 2 * math.pi * radius_int
     area = math.pi * (radius_int ** 2)
 
-    print("The circumference is: ", circumference,
-          ", and the area is: ", area)
+    print("The circumference = ", circumference, "\n and the area = ", area)
