1b0498b1a78b8fd7d86e8762a4be1c18730e502b
Ebuka <chukwunenyeebuka@gmail.com>
2022-10-06 11:46:53 +0100

    first commit

---
+++ calling_methods.py
@@ -0,0 +1,17 @@
+class CohortThirteen:
+    def __int__(self, __name, age):
+        self.name = __name
+        self.age = age
+
+    @staticmethod
+    def set_name(name):
+        return name
+
+    def set_age(self, name):
+        self.age = age
+
+    def get_name(self):
+    return self.name
+
+    def get_age(self):
+        retun self.age
