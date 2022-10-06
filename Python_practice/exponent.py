ee1fa6d4a2eded9e53b9f7b31d5cea826649c079
chinwoke <canugwara99@gmail.com>
2022-10-06 09:39:33 +0100

    stat

---
+++ Python_practice/exponent.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    # base = float(input("Enter a base"))
+    # exponent = int(input("Enter an exponent"))
+    # answer = base ** exponent
+    # print(f"{base} to the power of {exponent} = {answer}")
+    for n in range(1, 4):
+        for j in range(4, 7):
+            print(f"n = {n} and j = {j}")
